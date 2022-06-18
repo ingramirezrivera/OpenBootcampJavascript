61169f8608ffe98fa5a1647141a0b7ce8ea9644e
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-06-18 00:08:24 -0500

    Ejercicio Sintaxis

---
+++ SITANXIS/EJERCICIO/index.js
@@ -0,0 +1,23 @@
+// Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+// - Tu nombre (string)
+const nombre = "Daniel Ramirez";
+console.log(nombre);
+// - Tu edad (number)
+let edad = 36;
+console.log(edad);
+// - ¿Eres desarrollador? (boolean)
+let desarrollador = true;
+console.log(desarrollador);
+// - Tu fecha de nacimiento (Date)
+const fechaNacimiento = new Date(1986,08-1,11);
+console.log(fechaNacimiento);
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+const libroFavorito = {
+    titulo: "Estrategia del oceano azul",
+    autor: "W. Chan Kim,Renée Mauborgne", 
+    fecha: 2015,
+    url: "https://www.buscalibre.com.co/libro-la-estrategia-del-oceano-azul-w-chan-kim-renee-mauborgne-profit/9788416115891/p/47003168?gclid=Cj0KCQjwzLCVBhD3ARIsAPKYTcQSo5_yjsqmw-kPjo6v496CmyblsqvEUm0KhhlF5YWac3tTIgPwVcUaAofDEALw_wcB",
+}
+console.log(libroFavorito);
